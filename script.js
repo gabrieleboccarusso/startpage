c665c226b03a59e88885cf5bc930e1992a97bf4d
Gabriel <gabrieleboccarusso@gmail.com>
2021-01-19 19:32:09 +0100

    added the possibility to sign a film as done

--- script.js
+++ script.js
@@ -6,15 +6,22 @@ var ul = document.querySelector("ul");
 var film = document.getElementById("film-name");
 // we set the variable 'enter' to 13 to make to code more readable
 var enter = 13;
+// creating the button to strike through the watched films
+
 
 function addFilm () 
 {
 	// the function create a new li element for the list
 	var li = document.createElement("li");
 	// we append the value of the of the input with the film-name id the the li
-	li.appendChild(document.createTextNode(film.value + "<button> delete </button>"));
+	li.appendChild(document.createTextNode(film.value));
+	// we add the possibility to toggle between the class watched and no class
+	li.onclick = function () {
+		this.classList.toggle("watched");
+	};
 	// we append the li child with the name film inside the ul element
 	ul.appendChild(li);
+	// appending the button the sign the film as watched
 	// we reset the original value to zero
 	film.value = "";
 }
@@ -41,6 +48,5 @@ function addFilmAfterKeypress (event)
 }
 
 button.addEventListener("click", addFilmAfterClick);
-
 
 film.addEventListener("keypress", addFilmAfterKeypress);
